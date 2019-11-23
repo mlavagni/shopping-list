22f07614b0cdc0611fc33d47676b60a9c0de437a
mark <mark@marks-MBP.lan1>
2019-11-23 11:50:14 -0600

    added db conection and shopping center folder

---
+++ config/database.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/shopping-list',
+{
+    useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true
+})
+
+
+var db = mongoose.connection
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+})
